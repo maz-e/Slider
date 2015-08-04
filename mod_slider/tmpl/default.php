d425be1932c14803ad13ea68265515c72e733c81
Miguel Angel <m.zapes@gmail.com>
2015-08-04 16:50:04 +0200

    Finish template
    Adding the code for the module administration options working with the
    materialize framework

--- mod_slider/tmpl/default.php
+++ mod_slider/tmpl/default.php
@@ -7,10 +7,30 @@ defined('_JEXEC') or die;
       <?php for ($i=0; $i < 6 ; $i++) : ?>
          <?php if($params->get("show_slide_$i") == 1) : ?>
             <li>
-               <img src="<?php echo $params->get("imageslider_$i"); ?>" alt="<?php ?>" />
-               <div class="caption <?php ?> right-align">
-                  <h3 class="mega red-text"><?php echo $params->get("title_$i"); ?></h3>
-                  <h5 class="mega light grey-text text-lighten-5"><?php echo $params->get("slogan_$i"); ?></h5>
+               <img src="<?php echo $params->get("imageslider_$i"); ?>" alt="<?php echo $params->get("alt_text_$i"); ?>" />
+               <div class="caption <?php echo $params->get("text_direction_$i"); ?>">
+                  <?php if($params->get("url_slide_$i")) : ?>
+                     <a href="<?php echo $params->get("url_slide_$i"); ?>">
+                  <?php endif; ?>
+                     <h3 class="mega <?php echo $params->get("color_title_$i"); ?>-text">
+                        <?php echo $params->get("title_$i"); ?>
+                     </h3>
+                  <?php if($params->get("url_slide_$i")) : ?>
+                     </a>
+                  <?php endif; ?>
+                  <?php
+                  $mode = $params->get("color_slogan_$i",'light');
+                  switch ($mode) {
+                     case 'dark':
+                        echo '<h5 class="mega light grey-text text-darken-4">';
+                        break;
+                     case 'light':
+                     default:
+                        echo '<h5 class="mega light grey-text text-lighten-5">';
+                        break;
+                  } ?>
+                     <?php echo $params->get("slogan_$i"); ?>
+                  </h5>
                </div>
             </li>
          <?php endif; ?>
